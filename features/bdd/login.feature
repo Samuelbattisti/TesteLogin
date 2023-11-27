e20aa2b8fce6c88c36de815facece362b12df86a
Samuel <samuelbattisti10@gmail.com>
2023-11-27 18:20:51 -0300

    First commit

---
+++ features/bdd/login.feature
@@ -0,0 +1,12 @@
+#language: pt
+
+Funcionalidade: Sistema de Login
+Para ter acesso ao sistema
+O usuario do linkedin
+Deseja logar no site 
+
+Cenario: Login com sucesso
+Dado que o usuario queira logar
+Quando ele digitar as credenciais validas
+Entao deve acessar o site com sucesso
+
---
+++ features/pages/login.pages.rb
@@ -0,0 +1,12 @@
+class LoginPage < SitePrism::Page 
+    element :emailField, :id, "session_key"
+    element :passwordField, :id, "session_password"
+    element :loginButton, :xpath, "//*[@id='main-content']/section[1]/div/div/form/div[2]/button"
+
+    def userLogin
+        emailField.set "seuemail@gmail.com"
+        passwordField.set "Suasenha123"
+        loginButton.click
+    end
+
+end
